8b86eea7b037eb0396a5ccec1c85ad42bf4b73bb
Javier Ribal del Río <javierribal@gmail.com>
2025-07-19 23:08:57 +0200

    feat(client): Install shadcn

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   const [data, setData] = useState<{ message?: string }>({});
@@ -10,10 +11,17 @@ export default function Home() {
       .then((data) => setData(data));
   }, []);
 
+
+  const updateData = () => {
+    fetch('http://localhost:3001/api/data')
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  };
   return (
     <div>
       <h1>Frontend con Next.js</h1>
       <p>Datos del backend: {data.message}</p>
+      <Button onClick={updateData}>Actualizar Datos</Button>
     </div>
   );
 }
