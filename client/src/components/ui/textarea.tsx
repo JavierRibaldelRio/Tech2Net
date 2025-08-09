f981613f38fa0504824035e9171a128e51debae4
Javier Ribal del Río <javierribal@gmail.com>
2025-08-09 16:31:42 +0200

    feat: tweak button to have yellow color and textarea to have sizabletextarea

--- client/src/components/ui/textarea.tsx
+++ client/src/components/ui/textarea.tsx
@@ -15,4 +15,18 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
   )
 }
 
-export { Textarea }
+// The same text area but you can change de font size
+function TextareaSizable({ className, ...props }: React.ComponentProps<"textarea">) {
+  return (
+    <textarea
+      data-slot="textarea"
+      className={cn(
+        "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content w-full rounded-md border bg-transparent px-3 py-2 t shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 text-base h-full",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export { Textarea, TextareaSizable }
